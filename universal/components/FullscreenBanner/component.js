c0a78ae500aacffa57d34ecd7b2d4f8b8b3c40ff
Andrew Frank <afrank@spartansystems.co>
2016-10-20 09:55:59 -0400

    FullscreenBanner Component Created & boilerplate typography added.

---
+++ universal/components/FullscreenBanner/component.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './styles'
+
+type Props = {
+  title: String,
+  subTitle: String,
+  bannerText: Array,
+  bannerImage: String,
+};
+
+const defaultText = [`Morbi leo risus, porta ac consectetur ac, vestibulum at
+eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
+mattis consectetur purus sit amet fermentum.`]
+
+const FullscreenBanner = ({ title = 'Title', subTitle = 'Subtitle', bannerText = defaultText }: Props) => {
+  return (
+    <section className={styles.fullscreenBanner}>
+      <img className={styles.bannerImage} src='{bannerImage}' />
+      <div className={styles.bannerContent}>
+        <header className={styles.bannerHeader}>
+          <h1>{title}</h1>
+          <h2>{subTitle}</h2>
+        </header>
+        <section className={styles.bannerText}>
+          {bannerText.map(paragraph => (<p>{paragraph}</p>))}
+        </section>
+      </div>
+    </section>
+  )
+}
+
+export default FullscreenBanner
---
+++ universal/components/FullscreenBanner/component.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import HeroHeader from './component'
+
+describe('(Component) HeroHeader', () => {
+  it('should exist', () => {
+
+  })
+})
