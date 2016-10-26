9a36b1e520ef439ccc1bd9f48182d80ea6e5edd8
Andrew Frank <afrank@spartansystems.co>
2016-10-26 14:08:53 -0400

    Updated Home Page Layout

--- universal/layouts/Home/component.js
+++ universal/layouts/Home/component.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './styles'
 import MediumFeaturedPostBanner from '../../components/MediumFeaturedPostBanner'
-import MediumPostFilter from '../../components/MediumPostFilter'
 import MediumPostGrid from '../../components/MediumPostGrid'
 import FullWidthText from '../../components/FullWidthText'
 import EmailCapture from '../../components/EmailCapture'
@@ -11,10 +10,7 @@ import ThriveStoryCarousel from '../../components/ThriveStoryCarousel'
 export const HomeView = () => (
   <main role='main' className={styles.homePage}>
     <MediumFeaturedPostBanner />
-    <section className={styles.mediumPosts}>
-      <MediumPostFilter />
-      <MediumPostGrid />
-    </section>
+    <MediumPostGrid />
     <section className={styles.healthWellness}>
       <FullWidthText />
     </section>
