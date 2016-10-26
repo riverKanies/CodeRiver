aee4658cbe10fb7f5e97da18ff18f4b75f1da6ce
Andrew Frank <afrank@spartansystems.co>
2016-10-26 11:13:18 -0400

    Added in Pages for header.

--- universal/components/Header/component.js
+++ universal/components/Header/component.js
@@ -6,19 +6,54 @@ import mark from './assets/mark-color.png'
 
 export const Header = () => (
   <header role='banner' id='header' className={styles.container}>
+
     <span className={styles.thriveLogo}>
       <IndexLink to='/' activeClassName={styles.activeRoute}>
         <img src={mark} height='40' width='40' />
       </IndexLink>
     </span>
-    <nav role='navigation' className={styles.links}>
-      <Link id='about' to='/about' className={styles.link} activeClassName={styles.activeRoute}>
-        About
+
+    <nav role='navigation' className={styles.utilityNav}>
+      <section>
+        <Link to='/signup' className={styles.linkUtility} activeClassName={styles.activeRoute}>
+          Sign Up
+        </Link>
+        <Link to='javascript:void(0)' className={styles.linkUtility} activeClassName={styles.activeRoute}>
+          Search
+        </Link>
+      </section>
+    </nav>
+
+    <nav role='navigation' className={styles.mainNav}>
+      <Link id='navMedia' to='/media' className={styles.link} activeClassName={styles.activeRoute}>
+        Media
       </Link>
-      <Link to='/signup' className={styles.link} activeClassName={styles.activeRoute}>
-        Sign Up
+      <Link id='navLearn' to='/learn' className={styles.link} activeClassName={styles.activeRoute}>
+        Learn
+      </Link>
+      <Link id='navShop' to='http://shop.thriveglobal.com' className={styles.link} activeClassName={styles.activeRoute}>
+        Shop
+      </Link>
+      <Link id='navThrive' to='/thrive' className={styles.link} activeClassName={styles.activeRoute}>
+        Thrive
+      </Link>
+      <Link id='navContribute' to='javascript:void(0)' className={styles.link} activeClassName={styles.activeRoute}>
+        Contribute
       </Link>
     </nav>
+
+    <nav role='navigation' className={styles.socialNav}>
+      <Link to='javascript:void(0)' className={styles.linkSocial}>
+        Social
+      </Link>
+      <Link to='javascript:void(0)' className={styles.linkSocial}>
+        Social
+      </Link>
+      <Link to='javascript:void(0)' className={styles.linkSocial}>
+        Social
+      </Link>
+    </nav>
+
   </header>
 )
 
