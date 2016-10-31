42102c26155fa7c4f8b6151f968070f35334b20d
Jonathan Barber <jbarber@spartansystems.co>
2016-10-31 13:50:47 -0400

    styles for behavior change section

--- universal/components/BehaviorChangeSection/component.js
+++ universal/components/BehaviorChangeSection/component.js
@@ -3,16 +3,39 @@ import styles from './styles'
 import IconWithText from 'components/IconWithText'
 
 type Props = {
-  title: String
+  title: String,
+  details: Array<string>,
 };
 
-const BehaviorChangeSection = ({ title = 'BehaviorChangeSection' }: Props) => {
+const details = [
+  `Making a sustained behavioral change happens when we no longer feel we’re
+  having to work to do something other than what we’d normally do.
+  That is, it’s when we find a new equilibrium and create a new norm.`
+]
+
+const BehaviorChangeSection = ({
+  title = 'Behavior Change', }: Props) => {
   return (
     <section className={styles.container}>
       <h2>{title}</h2>
+      {details.map(paragraph => (<p className={styles.details}>{paragraph}</p>))}
       <section className={styles.row}>
-        <IconWithText />
-        <IconWithText />
+        <IconWithText description='This is a mini description' />
+        <IconWithText description='Cras mattis consectetur purus sit amet fermentum
+         Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+         ut fermentum massa justo sit amet risus.'
+        />
+        <IconWithText description='This is a medium length description.
+        tortor mauris condimentum nibh. onec ullamcorper nulla non.'
+        />
+        <IconWithText description='This is a mini description' />
+        <IconWithText description='Cras mattis consectetur purus sit amet fermentum
+         Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+         ut fermentum massa justo sit amet risus.'
+        />
+        <IconWithText description='This is a medium length description.
+        tortor mauris condimentum nibh. onec ullamcorper nulla non.'
+        />
       </section>
     </section>
   )
