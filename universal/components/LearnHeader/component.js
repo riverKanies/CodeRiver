74fe6551188b48b3fefd3566cfed2cd26b70bd74
Jonathan Barber <jbarber@spartansystems.co>
2016-11-01 12:04:23 -0400

    Styles for LearnHeader
    _Why_?
    Need a full-width background image component that only covers half the vertical viewport.
    
    _How_?
    
    *Added styles
    
    _Side Effects_?
    None

--- universal/components/LearnHeader/component.js
+++ universal/components/LearnHeader/component.js
@@ -3,14 +3,21 @@ import React from 'react'
 import styles from './styles'
 
 type Props = {
-  title: string
+  title: string,
 }
+
+const details = [
+  `These are the details`
+]
 
 const LearnHeader = ({ title = 'LearnHeader' }: Props) => {
   return (
-    <div className={styles.container}>
-      {title}
-    </div>
+    <section className={styles.container}>
+      <section className={styles.content}>
+        <h1>{title}</h1>
+        <p>{details}</p>
+      </section>
+    </section>
   )
 }
 
--- universal/layouts/Learn/component.js
+++ universal/layouts/Learn/component.js
@@ -1,12 +1,14 @@
 /* @flow */
 import React from 'react'
 import styles from './styles'
+import LearnHeader from 'components/LearnHeader'
 import PathwaySection from 'components/PathwaySection'
 import BehaviorChangeSection from 'components/BehaviorChangeSection'
 
 export const LearnView = () => (
   <section className={styles.container}>
     <section className={styles.content}>
+      <LearnHeader />
       <PathwaySection />
       <BehaviorChangeSection />
     </section>
