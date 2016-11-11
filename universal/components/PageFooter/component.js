7b6585dc216398ab69e1977ba9d836f9ddd3d390
beauingfield <beau.wingfield@gmail.com>
2016-11-11 14:50:39 -0500

    quote added

--- universal/components/PageFooter/component.js
+++ universal/components/PageFooter/component.js
@@ -3,13 +3,29 @@ import React from 'react'
 import styles from './styles'
 
 type Props = {
-  title: string
-}
+  title: string,
+  quote: string,
+  citation: string,
+  quoteImage: string,
+  quote: Array<string>,
+  citation: string,
+  quoteImage: any
+};
+
+const footerQuote = [`What is a good life?” has been a question asked by
+philosophers going back to the ancient Greeks. But somewhere along the line we
+abandoned the question and shifted our attention to how much money we can make,
+how big a house we can buy, and how high we can climb up the career ladder.
+This idea of success can work-or at least appear to work – in the short term.
+But over the long term, money and power by themselves are like a two-legged
+stool – you can balance on them for a while, but eventually you’re going to
+topple over. More and more people – very successful people – are toppling over.`]
 
 const PageFooter = ({ title = 'PageFooter' }: Props) => {
   return (
     <div className={styles.container}>
       {title}
+
     </div>
   )
 }
