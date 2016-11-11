0f7781ad8692ef8a4d2569e87c967fb144c90241
Andrew Frank <afrank@spartansystems.co>
2016-11-11 12:46:28 -0500

    Initial Commit

--- universal/components/EmailCapture/component.js
+++ universal/components/EmailCapture/component.js
@@ -1,29 +1,18 @@
-/* @flow */
 import React from 'react'
 import styles from './styles'
-import LeadForm from 'containers/LeadForm'
-// Add in a button
+// import LeadForm from 'containers/LeadForm'
 
 type Props = {
   title: string,
-  sectionText: Array<string>,
 };
 
-const defaultText = [`Morbi leo risus, porta ac consectetur ac, vestibulum at
-eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
-mattis consectetur purus sit amet fermentum.`]
-
-const EmailCapture = ({ title = 'Title', sectionText = defaultText }: Props) => {
+const EmailCapture = ({ title = 'Thrive with us today!' }: Props) => {
   return (
     <section className={styles.emailCapture}>
       <div className={styles.container}>
         <header>
-          <h3>{title}</h3>
+          <h2>{title}</h2>
         </header>
-        <section className={styles.sectionContent}>
-          {sectionText.map((paragraph, index) => <p key={index}>{paragraph}</p>)}
-          <LeadForm />
-        </section>
       </div>
     </section>
   )
