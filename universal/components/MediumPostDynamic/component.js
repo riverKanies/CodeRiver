0c2433b96e70a91d7e4a0c2204fdeac48ba18137
Andrew Frank <afrank@spartansystems.co>
2016-11-14 16:32:56 -0500

    In process of styling. Lots of dynamic composing.

--- universal/components/MediumPostDynamic/component.js
+++ universal/components/MediumPostDynamic/component.js
@@ -5,11 +5,11 @@ import dummyImage from './assets/postFPO.jpg'
 import ContentLabel from 'components/ContentLabel'
 
 type Props = {
-  cardType: 'half' | 'quarter',
+  cardType: 'half' | 'halfImage' | 'quarterWhite' | 'quarterBlue',
   postTitle: string,
   postAuthor: string,
   hasImage: 'imageTrue' | 'imageFalse',
-  hasSynopsis: 'true' | 'false',
+  hasSynopsis: 'synopsisTrue' | 'synopsisFalse',
   postSynopsis: Array<string>,
   postLabel: Object,
   postURL: 'string',
@@ -21,11 +21,11 @@ const postImage = dummyImage
 const defaultText = 'When providing a health benefits strategy, employers too often focus solely on physical health.'
 
 const MediumPostDynamic = ({
-  cardType = 'half',
+  cardType = 'halfImage',
   postTitle = 'Post Title',
   postAuthor = 'Post Author',
   hasImage = 'imageTrue',
-  hasSynopsis = 'true',
+  hasSynopsis = 'synopsisTrue',
   postSynopsis = defaultText,
   postLabel = {},
   postURL = 'javascript:void(0)'
@@ -34,19 +34,21 @@ const MediumPostDynamic = ({
     <figure className={styles[cardType]}>
       <a className={styles.cardLink} href={postURL}>
         <ContentLabel {...postLabel} />
-        <figcaption className={styles.postMeta}>
-          <header className={styles.header}>
-            <h3>{postTitle}</h3>
-          </header>
-          <section className={styles[hasSynopsis]}>
-            <p>{postSynopsis}</p>
-          </section>
-          <footer className={styles[cardType]}>
-            <span className={styles.authorWrap}>
-              <p><i>By</i> {postAuthor}</p>
-            </span>
-          </footer>
-        </figcaption>
+        <section className={styles.cardContent}>
+          <figcaption className={styles.postMeta}>
+            <header className={styles.header}>
+              <h3>{postTitle}</h3>
+            </header>
+            <section className={styles[hasSynopsis]}>
+              <p>{postSynopsis}</p>
+            </section>
+            <footer className={styles.footer}>
+              <span className={styles.authorWrap}>
+                <p><i>By</i> {postAuthor}</p>
+              </span>
+            </footer>
+          </figcaption>
+        </section>
         <span className={styles[hasImage]}>
           <img className={styles.image} src={postImage} alt={postTitle} />
         </span>
