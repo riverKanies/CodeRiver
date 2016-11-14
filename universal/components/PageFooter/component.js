604bd43cc2c980f92873512effed1487926bceb3
beauingfield <beau.wingfield@gmail.com>
2016-11-14 09:37:29 -0500

    footer columns setup

--- universal/components/PageFooter/component.js
+++ universal/components/PageFooter/component.js
@@ -6,26 +6,45 @@ type Props = {
   title: string,
   quote: string,
   citation: string,
-  quoteImage: string,
   quote: Array<string>,
-  citation: string,
-  quoteImage: any
+  heading: string
 };
 
-const footerQuote = [`What is a good life?” has been a question asked by
-philosophers going back to the ancient Greeks. But somewhere along the line we
-abandoned the question and shifted our attention to how much money we can make,
-how big a house we can buy, and how high we can climb up the career ladder.
-This idea of success can work-or at least appear to work – in the short term.
-But over the long term, money and power by themselves are like a two-legged
-stool – you can balance on them for a while, but eventually you’re going to
-topple over. More and more people – very successful people – are toppling over.`]
+const footerQuote = [`Time is the most valuable coin in your life. 
+You and you alone will determine how that coin will be spent.
+Be careful that you do not let other people spend it for you.`]
 
-const PageFooter = ({ title = 'PageFooter' }: Props) => {
+const PageFooter = ({
+	quote = footerQuote,
+  citation = 'Carl Sandburg',
+	heading = 'Links',
+	heading2 = 'Follow Us'
+}: Props) => {
   return (
     <div className={styles.container}>
-      {title}
-
+      <section className={styles.quoteContainer}>
+	      <div className={styles.container}>
+	        <section className={styles.footerQuote}>
+	          {quote.map((paragraph, index) => <p key={index}>{quote}</p>)}
+	          <cite>{citation}</cite>
+	        </section>
+	      </div>
+    	</section>
+    	<section className={styles.linksContainer}>
+	      <div className={styles.heading}>
+	      	{heading}
+	      </div>
+	      <ul>
+	      	<li className={styles.footerLinks}>
+	          Press
+	      	</li>
+	      </ul>
+    	</section>
+    	<section className={styles.socialContainer}>
+	      <div className={styles.heading}>
+	      	{heading2}
+	      </div>
+    	</section>
     </div>
   )
 }
