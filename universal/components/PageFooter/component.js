76f96bb90f605d09cccad57cc444e25eda9f72bc
beauingfield <beau.wingfield@gmail.com>
2016-11-14 11:33:40 -0500

    logo

--- universal/components/PageFooter/component.js
+++ universal/components/PageFooter/component.js
@@ -1,6 +1,8 @@
 /* @flow */
 import React from 'react'
 import styles from './styles'
+import logo from './assets/logo-footer.png'
+import { Link } from 'react-router'
 
 type Props = {
   title: string,
@@ -14,22 +16,21 @@ const footerQuote = [`Time is the most valuable coin in your life.
 You and you alone will determine how that coin will be spent.
 Be careful that you do not let other people spend it for you.`]
 
+const logoFooter = logo
+
 const PageFooter = ({
 	quote = footerQuote,
-  citation = 'Carl Sandburg',
+  citation = '- Carl Sandburg',
 	heading = 'Links',
-	heading2 = 'Follow Us'
+	heading2 = 'Follow Us',
+	logoFooter = logo
 }: Props) => {
   return (
-    <div className={styles.container}>
-      <section className={styles.quoteContainer}>
-	      <div className={styles.container}>
-	        <section className={styles.footerQuote}>
-	          {quote.map((paragraph, index) => <p key={index}>{quote}</p>)}
-	          <cite>{citation}</cite>
-	        </section>
-	      </div>
-    	</section>
+    <div className={styles.footerContainer}>
+      <section className={styles.footerQuote}>
+        {quote.map((paragraph, index) => <p key={index}>{quote}</p>)}
+        <cite>{citation}</cite>
+      </section>
     	<section className={styles.linksContainer}>
 	      <div className={styles.heading}>
 	      	{heading}
@@ -38,12 +39,39 @@ const PageFooter = ({
 	      	<li className={styles.footerLinks}>
 	          Press
 	      	</li>
+	      	<li className={styles.footerLinks}>
+	          Become a Contributor
+	      	</li>
+	      	<li className={styles.footerLinks}>
+	          Advertise
+	      	</li>
+	      	<li className={styles.footerLinks}>
+	          Careers
+	      	</li>
+	      	<li className={styles.footerLinks}>
+	          Site Map
+	      	</li>
+	      	<li className={styles.footerLinks}>
+	          Contact
+	      	</li>
 	      </ul>
     	</section>
     	<section className={styles.socialContainer}>
 	      <div className={styles.heading}>
 	      	{heading2}
 	      </div>
+	      <div className={styles.logo}>
+	      	{logoFooter}
+	      </div>
+    	</section>
+    	<section className={styles.copyright}>
+    		Thrive Global © 2016  |
+    		<Link to='/privacy' className={styles.link}>
+        	Privacy
+        </Link> &
+				<Link to='/terms' className={styles.link}>
+        	Terms
+        </Link>
     	</section>
     </div>
   )
