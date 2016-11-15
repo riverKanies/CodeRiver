680a7ae95d3f55c589d2666ac339a572951b7380
Andrew Frank <afrank@spartansystems.co>
2016-11-15 15:02:20 -0500

    added story

--- universal/components/EmailCaptureMini/stories.js
+++ universal/components/EmailCaptureMini/stories.js
@@ -7,16 +7,22 @@ import EmailCaptureMini from './component'
 
 const notes = 'This story demonstrates the component EmailCaptureMini'
 
-// const props = {
-//   title: 'Thrive with us today!'
-// }
+const defaultText = ['Sign up to receive the best tips and articles right in your email.']
+
+const props = {
+  title: 'Thrive with us today!',
+  message: defaultText
+}
 
 storiesOf('EmailCaptureMini', module)
   .addDecorator(withKnobs)
   .add('with props', () => {
     return (
       <WithNotes notes={notes}>
-        <EmailCaptureMini />
+        <EmailCaptureMini
+          title={text('Email Capture Header', props.title)}
+          message={text('Message', props.message)}
+        />
       </WithNotes>
     )
   })
