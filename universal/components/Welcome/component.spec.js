2982fe9dc2c500d2b4da5887f9bf68df1299642a
Travis Palmer <palmer.travis@gmail.com>
2016-11-16 11:02:09 -0500

    Welcome component spec

---
+++ universal/components/Welcome/component.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Welcome } from './component'
+import { shallow, mount } from 'enzyme'
+
+describe('(Component) Welcome', () => {
+  it('should render a <Profile /> element', () => {
+    const props = {
+      location: {
+        query: 'test'
+      }
+    }
+
+    const wrapper = shallow(<Welcome {...props} />)
+    const element = wrapper.find('Connect(ProfileContainer)')
+
+    expect(element.length).toBe(1)
+  })
+})
