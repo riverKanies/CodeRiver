38d35839fabd16e66a7ec9a72e446d7e9aef4607
Travis Palmer <palmer.travis@gmail.com>
2016-11-16 10:31:37 -0500

    GenericFormError spec

--- universal/components/GenericFormError/component.spec.js
+++ universal/components/GenericFormError/component.spec.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import GenericFormError from './component'
+import { shallow } from 'enzyme'
 
 describe('(Component) GenericFormError', () => {
-  it('should exist', () => {
+  it('should render passed errors', () => {
+    const errors = { error: 'there was a form error' }
+    const wrapper = shallow(<GenericFormError {...errors} />)
+    const element = wrapper.find('div')
 
+    expect(element.contains('there was a form error')).toBe(true)
   })
 })
