bf705b62e391b944be82fac041e65d012b01af52
Andrew Frank <afrank@spartansystems.co>
2016-11-17 10:08:27 -0500

    ready to PR

--- universal/components/HalfTextOverlay/component.js
+++ universal/components/HalfTextOverlay/component.js
@@ -5,9 +5,10 @@ import ContentLabel from 'components/ContentLabel'
 import dummyImage from './assets/arrianaH.jpg'
 
 type Props = {
-  label: Object,
+  postLabel: Object,
   title: string,
   sectionText: Array<string>,
+  url: string,
   bgImage: string
 }
 
@@ -15,27 +16,37 @@ const defaultText = [`Morbi leo risus, porta ac consectetur ac, vestibulum at
 eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
 mattis consectetur purus sit amet fermentum.`]
 
+const defaultLabel = {
+  label: 'Label',
+  labelColor: 'whiteLabel'
+}
+
 const HalfTextOverlay = ({
-  label = {},
+  postLabel = defaultLabel,
   title = 'Headline',
   sectionText = defaultText,
+  url = 'javascript:void(0)',
   bgImage = dummyImage
 }: Props) => {
   return (
-    <section className={styles.halfOverlay}>
-      <span className={styles.imageWrap}>
-        <img className={styles.image} src={bgImage} alt={title} />
-      </span>
-      <section className={styles.postContent}>
-        <ContentLabel {...label} />
-        <div className={styles.contentWrap}>
-          <section className={styles.content}>
-            <header className={styles.header}>
-              <h2>{title}</h2>
-            </header>
-            {sectionText.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
-          </section>
-        </div>
+    <section className={styles.gridWrap}>
+      <section className={styles.halfOverlay}>
+        <span className={styles.imageWrap}>
+          <a href={url}><img className={styles.image} src={bgImage} alt={title} /></a>
+        </span>
+        <section className={styles.postContent}>
+          <ContentLabel {...postLabel} />
+          <div className={styles.contentWrap}>
+            <section className={styles.content}>
+              <a href={url}>
+                <header className={styles.header}>
+                  <h2>{title}</h2>
+                </header>
+                {sectionText.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
+              </a>
+            </section>
+          </div>
+        </section>
       </section>
     </section>
   )
