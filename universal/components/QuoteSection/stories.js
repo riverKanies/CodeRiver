5e1432509c81c77d82c4932bd5b26b72fdc7f43a
Andrew Frank <afrank@spartansystems.co>
2016-11-17 12:44:21 -0500

    Storybooked

--- universal/components/QuoteSection/stories.js
+++ universal/components/QuoteSection/stories.js
@@ -2,17 +2,34 @@ import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { WithNotes } from '@kadira/storybook-addon-notes'
 import { withKnobs, text, select } from '@kadira/storybook-addon-knobs'
+import dummyImage from './assets/quote_imagePlaceholder.png'
 
 import QuoteSection from './component'
 
 const notes = 'This story demonstrates the props that can be passed to thw QuoteSection component.'
+
+const bgColor = [
+  'default',
+  'white',
+  'gray'
+]
+
+const defaultQuote = [
+  `Intention is one of the most powerful forces there is.
+  What you mean when you do a thing will always determine the outcome.`
+]
 
 storiesOf('QuoteSection', module)
   .addDecorator(withKnobs)
   .add('with props', () => {
     return (
       <WithNotes notes={notes}>
-        <QuoteSection />
+        <QuoteSection
+          bgColor={select('Background Color', bgColor, 'default')}
+          quote={text('Quote Text', defaultQuote)}
+          citation={text('Cite', 'Arriana Huffington')}
+          quoteImage={text('Quote Image', dummyImage)}
+        />
       </WithNotes>
     )
   })
