63956753ac1f3b43f3f280a038f2ffa65281f3e0
Jonathan Barber <jbarber@spartansystems.co>
2016-11-17 15:54:05 -0500

    Changed component name

--- universal/components/FeaturedStory/stories.js
+++ universal/components/FeaturedStory/stories.js
@@ -3,20 +3,20 @@ import { storiesOf } from '@kadira/storybook'
 import { WithNotes } from '@kadira/storybook-addon-notes'
 import { withKnobs, text } from '@kadira/storybook-addon-knobs'
 
-import StoryComponent from './component'
+import FeaturedStory from './component'
 
-const notes = 'This story demonstrates the props that can be passed to StoryComponent'
+const notes = 'This story demonstrates the props that can be passed to FeaturedStory'
 
 const props = {
-  title: 'StoryComponent Title'
+  title: 'FeaturedStory Title'
 }
 
-storiesOf('StoryComponent', module)
+storiesOf('FeaturedStory', module)
   .addDecorator(withKnobs)
   .add('with props', () => {
     return (
       <WithNotes notes={notes}>
-        <StoryComponent
+        <FeaturedStory
           title={text('Title', props.title)}
         />
       </WithNotes>
