1c3126a286813924de378907a8c13bc214b2f9c0
Jonathan Barber <jbarber@spartansystems.co>
2016-11-17 14:45:55 -0500

    base styles

--- universal/components/StoryComponent/component.js
+++ universal/components/StoryComponent/component.js
@@ -1,16 +1,45 @@
 /* @flow */
 import React from 'react'
 import styles from './styles'
+import Button from 'components/Button'
 
 type Props = {
+  author: string,
+  details: string,
   title: string
 }
 
-const StoryComponent = ({ title = 'StoryComponent' }: Props) => {
+const defaultDetails = [`this the details Cras justo odio, dapibus ac facilisis in,
+egestas eget quam. Etiam porta sem malesuada magna mollis euismod.`]
+
+const defaultTitle = ['Its time to say goodbye']
+
+const defaultAuthor = ['Koby Bryant']
+
+const StoryComponent = ({
+  title = defaultTitle,
+  author = defaultAuthor,
+  details = defaultDetails
+}: Props) => {
   return (
-    <div className={styles.container}>
-      {title}
-    </div>
+    <section className={styles.container}>
+      <section className={styles.container}>
+        <label className={styles.label}>STORIES</label>
+        <h1 className={styles.header}>{title}</h1>
+        <p className={styles.details}>
+          {details}
+          <span className={styles.author}>by</span>
+          {author}
+        </p>
+        <div className={styles.button}>
+          <Button
+            buttonText='READ STORY'
+            linkTo='/'
+            buttonStyle='white'
+          />
+        </div>
+      </section>
+    </section>
   )
 }
 
