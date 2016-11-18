fa9413487dfb7c69bd33e5e814d5c72e7249eebc
beauingfield <beau.wingfield@gmail.com>
2016-11-18 13:16:30 -0500

    sidebar styles

--- universal/components/Sidebar/component.js
+++ universal/components/Sidebar/component.js
@@ -17,7 +17,7 @@ const Sidebar = ({
   return (
     <aside role='sidebar' className={styles.sidebar}>
       <div className='sidebarContainer'>
-        <header>
+        <header className='sidebar'>
           <h3>{title}</h3>
         </header>
         <ul>
