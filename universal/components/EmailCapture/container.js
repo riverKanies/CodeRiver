e87aadb6b5e7da48ab1cf191b22e16f1db2cef27
Bill Brock <william.e.brock@gmail.com>
2016-11-20 12:54:03 -0500

    Wired mocked out forms to analytics middleware
    _Why_?
    
    we need captured emails to be pushed to segment and then to mailchimp
    
    _How_?
    
    * created well defined actions in the analytics middleware
    * updated all usage of analytics actions to import the specific actions
      rather than having magic strings floating around
    * created a new redux input field, basicinput, that displays no errors
      or warnings
    * refactored both email forms to use new field
    * wired both forms to redux form
    
    _Side Effects_? / Future enhancements
    
    * email forms could most likely be combined into one reusable component

--- universal/components/EmailCapture/container.js
+++ universal/components/EmailCapture/container.js
@@ -1,8 +1,7 @@
-import { reduxForm } from 'redux-form'
 import { actions } from 'modules/Middleware/analytics'
+import { reduxForm } from 'redux-form'
 import { createValidator, required, email } from 'lib/validate'
-
-import LeadForm from './component'
+import Component from './component'
 
 const validate = createValidator({
   email: [required, email]
@@ -10,14 +9,17 @@ const validate = createValidator({
 
 const handleSubmit = (values, dispatch) => {
   // dispatch event to be caught by middleware
-  dispatch({ type: actions.leadFormSubmitted, track: { email: values.email } })
+  dispatch({
+    type: actions.leadFormSubmitted,
+    track: { email: values.email }
+  })
 
   // reset the lead form
   values.email = ''
 }
 
 export default reduxForm({
-  form: 'lead-form',
+  form: 'email-capture',
   onSubmit: handleSubmit,
   validate
-})(LeadForm)
+})(Component)
--- universal/components/EmailCaptureMini/component.spec.js
+++ universal/components/EmailCaptureMini/component.spec.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import EmailCaptureMini from './component'
+import { shallow } from 'enzyme'
 
-describe('(Component) EmailCaptureMini', () => {
-  it('should exist', () => {
+import EmailCaptureMini from './container'
 
+describe('(Container) EmailCaptureMini', () => {
+  const wrapper = shallow(<EmailCaptureMini />)
+
+  it('should require a valid email', () => {
+    const expectedResult = wrapper.props().validate()
+
+    expect(Object.keys(expectedResult)).toContain('email')
+  })
+
+  it('should accept a valid email', () => {
+    const expectedResult = wrapper.props().validate({'email': 'test@test.com'})
+
+    expect(Object.keys(expectedResult)).not.toContain('email')
   })
 })
