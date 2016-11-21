d07ab128f7d766f7567ced2510eaa205a128c016
beauingfield <beau.wingfield@gmail.com>
2016-11-21 16:33:15 -0500

    new component created

---
+++ universal/components/AppFullWidthTwoColLeft/component.js
@@ -0,0 +1,49 @@
+/* @flow */
+import React from 'react'
+import styles from './styles'
+import dummyImage from './assets/appPlaceholder.png'
+import Button from 'components/Button'
+
+type Props = {
+  sectionClass: 'default' | 'lightGray',
+  title: string,
+  label: Object,
+  sectionText: Array<string>,
+  image: string,
+  button: Object
+};
+
+const defaultText = [`Morbi leo risus, porta ac consectetur ac, vestibulum at
+eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
+mattis consectetur purus sit amet fermentum.`]
+
+const AppFullWidthTwoColLeft = ({
+  sectionClass = 'default',
+  title = 'Title',
+  sectionText = defaultText,
+  image = dummyImage,
+  button = {}
+}: Props) => {
+  return (
+    <section className={styles[sectionClass]}>
+      <section className={styles.container}>
+        <div className={styles.column}>
+          <figure className={styles.imageWrap}>
+            <img className={styles.image} src={image} alt={title} />
+          </figure>
+        </div>
+        <div className={styles.column}>
+          <section className={styles.sectionContent}>
+            <header className={styles.header}>
+              <h2>{title}</h2>
+            </header>
+            {sectionText.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
+            <Button {...button} />
+          </section>
+        </div>
+      </section>
+    </section>
+  )
+}
+
+export default AppFullWidthTwoColLeft
