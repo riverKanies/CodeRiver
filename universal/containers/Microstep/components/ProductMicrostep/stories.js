7ccf7e39b613208f36cb23da2f1547d0f11ac1f6
Andrew Frank <afrank@spartansystems.co>
2016-11-21 10:29:05 -0500

    style updates. almost done.

--- universal/containers/Microstep/components/ProductMicrostep/stories.js
+++ universal/containers/Microstep/components/ProductMicrostep/stories.js
@@ -11,14 +11,15 @@ import ProductMicrostep from './component'
 const notes = 'This story demonstrates the props that can be passed to LinkMicrostep'
 
 const markdown = `
-
+Get a beautiful analog alarm clock for your bedside table to replace
+your iPhone as your alarm.
 `
 
 const props = {
   microstep: {
-    title: 'product title',
+    title: 'Product Title',
     body_html: marked(markdown),
-    link: 'http://www.google.com',
+    link: 'javascript:void(0)',
     price: '100.00',
     image: {
       src: 'http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg'
