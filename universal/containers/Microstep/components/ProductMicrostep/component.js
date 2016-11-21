abdaca9bd921b7cf519001ecb8e6b9516f012880
Andrew Frank <afrank@spartansystems.co>
2016-11-21 10:29:05 -0500

    style updates. almost done.

--- universal/containers/Microstep/components/ProductMicrostep/component.js
+++ universal/containers/Microstep/components/ProductMicrostep/component.js
@@ -12,31 +12,41 @@ type Props = {
     image: {
       src: string
     }
+  },
+  button: {
+    buttonText: string,
+    buttonStyle: string
   }
 };
 
 const ProductMicrostep = ({ microstep }: Props) => {
-  const {
-    cta_text = 'Learn More'
-  } = microstep
   return (
     <div className={styles.container}>
+
       <section className={styles.content}>
+
         <header className={styles.header}>
           <h5 className={styles.label}>We Recommend</h5>
           <h1 className={styles.msTitle}>{microstep.title}</h1>
         </header>
+
         <span className={styles.productBy}>
           <h4><em>by</em> NAME</h4>
         </span>
+
+        <div dangerouslySetInnerHTML={{__html: microstep.body_html}} />
+
         <img src={microstep.image.src} alt={microstep.title} />
         <p>{microstep.price}</p>
-        <div dangerouslySetInnerHTML={{__html: microstep.body_html}} />
-        <Button>
-          buttonText: {cta_text},
-          linkTo: {microstep.link},
-          buttonStyle: 'violet'
-        </Button>
+
+        <div className={styles.button}>
+          <Button>
+            buttonText: 'Learn More',
+            linkTo: {microstep.link},
+            buttonStyle: 'violet'
+          </Button>
+        </div>
+
       </section>
     </div>
   )
@@ -51,6 +61,10 @@ ProductMicrostep.defaultProps = {
     image: {
       src: '/path/to/image'
     }
+  },
+  button: {
+    buttonText: 'Learn More',
+    buttonStyle: 'violet'
   }
 }
 
