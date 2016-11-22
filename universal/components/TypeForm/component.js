fb333145238d551c8c4cb0567155a386f137caea
Omar Niazi <ramoizain@gmail.com>
2016-11-21 20:56:13 -0500

    More changes to form

--- universal/components/TypeForm/component.js
+++ universal/components/TypeForm/component.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-const typeFormURL = (key) => {
+const typeFormURL = (key, urlParams = '') => {
   const creds = JSON.parse(window.localStorage.getItem('thrive_user_headers'))
-  const url = `https://thriveglobal.typeform.com/to/${key}?typeform-embed=embed-fullpage`
-
-  return `${url}&accesstoken=${creds['access-token']}&uid=${creds.uid}&client=${creds.client}`
+  return `https://thriveglobal.typeform.com/to/${key}?typeform-embed=embed-fullpage${urlParams}`
 }
 
 const styles = {
@@ -17,13 +15,13 @@ type Props = {
 }
 
 // Engage Page Layout
-export const TypeForm = ({ typeformkey } : Props) => (
+export const TypeForm = ({ typeformkey, urlParams } : Props) => (
   <main role='main'>
     <iframe
       style={styles}
       id='typeform-full'
       frameBorder='0'
-      src={typeFormURL(typeformkey)}
+      src={typeFormURL(typeformkey, urlParams)}
     />
   </main>
 )
