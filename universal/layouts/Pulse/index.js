24de563ef689bddfba9b57f31b9f86d5a9604bc1
Omar Niazi <ramoizain@gmail.com>
2016-11-21 20:56:13 -0500

    More changes to form

--- universal/layouts/Pulse/index.js
+++ universal/layouts/Pulse/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import TypeForm from 'components/TypeForm'
+import styles from './styles'
 
 // Engage Page Layout
 export default function PulseView () {
- return <TypeForm typeformkey='sKzAso' />
+  const creds = JSON.parse(window.localStorage.getItem('thrive_user_headers'))
+  
+  if (!creds || creds && !creds.uid) return (
+    <div className={styles.PulseLogin}>
+      <h3> You must be logged in to do assessment </h3>
+    </div>
+  )
+
+
+  return <TypeForm typeformkey='sKzAso' urlParams={`&accesstoken=${creds['access-token']}&uid=${creds.uid}&client=${creds.client}`} />
 }
 
 
