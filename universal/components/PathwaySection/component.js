d50ab0e2dd876d37353285783c5817edf4457567
Andrew Frank <afrank@spartansystems.co>
2016-11-22 17:28:37 -0500

    learn page done

--- universal/components/PathwaySection/component.js
+++ universal/components/PathwaySection/component.js
@@ -5,15 +5,18 @@ import PathwayCard from 'components/PathwayCard'
 import productivityImage from '../../assets/thrivin.jpg'
 
 type Props = {
-  title: 'titleTrue' | 'titleFalse',
+  title: string,
   pathways: Array<any>
 };
 
-const PathwaySection = ({ title = 'Pathways', pathways = [] }: Props) => {
+const PathwaySection = ({
+  title = 'Pathways',
+  pathways = []
+}: Props) => {
   return (
     <section className={styles.container}>
       <section className={styles.content}>
-        <header className={styles[title]}>
+        <header className={styles.title}>
           <h2>{title}</h2>
         </header>
         <section className={styles.row}>
