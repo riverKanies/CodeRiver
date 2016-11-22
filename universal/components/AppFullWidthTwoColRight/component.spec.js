5c618a86fa26a2e854359e5730efb748417fb06a
beauingfield <beau.wingfield@gmail.com>
2016-11-22 10:42:28 -0500

    stylin'

---
+++ universal/components/AppFullWidthTwoColRight/component.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import FullWidthTwoColRight from './component'
+
+describe('(Component) FullWidthTwoCol', () => {
+  it('should exist', () => {
+
+  })
+})
--- universal/components/AppFullWidthTwoColRight/stories.js
+++ universal/components/AppFullWidthTwoColRight/stories.js
@@ -3,11 +3,11 @@ import { storiesOf } from '@kadira/storybook'
 import { WithNotes } from '@kadira/storybook-addon-notes'
 import { withKnobs, text, select } from '@kadira/storybook-addon-knobs'
 
-import AppFullWidthTwoColLeft from './component'
+import AppFullWidthTwoColRight from './component'
 import dummyImage from './assets/appPlaceholder.png'
 
 const notes = `This story demonstrates the props that can be passed to
-AppFullWidthTwoColLeft.`
+AppFullWidthTwoColRight.`
 
 const defaultText = [`Morbi leo risus, porta ac consectetur ac, vestibulum at
 eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
@@ -26,12 +26,12 @@ const props = {
   image: dummyImage
 }
 
-storiesOf('AppFullWidthTwoColLeft', module)
+storiesOf('AppFullWidthTwoColRight', module)
   .addDecorator(withKnobs)
   .add('with props', () => {
     return (
       <WithNotes notes={notes}>
-        <AppFullWidthTwoColLeft
+        <AppFullWidthTwoColRight
           sectionClass={select('Section Class', props.sectionClass, props.sectionClass[0])}
           title={text('Section Title', props.title)}
           sectionText={text('Section Text', props.sectionText)}
