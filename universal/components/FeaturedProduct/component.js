ff69d379ac7208f9402c7b37d86e3948235aa968
beauingfield <beau.wingfield@gmail.com>
2016-11-23 11:46:04 -0500

    component updated

--- universal/components/FeaturedProduct/component.js
+++ universal/components/FeaturedProduct/component.js
@@ -1,26 +1,33 @@
 /* @flow */
 import React from 'react'
 import styles from './styles'
-import image from './assets/product-placeholder.jpg'
+import image from './assets/Muse_black-square.jpg'
+import image2 from './assets/sense-square.jpg'
+import image3 from './assets/thrive-pillow-square.jpg'
+import image4 from './assets/phone-bed-square.jpg'
 
 type Props = {
   productTitle: string,
-  price: string
+  price: string,
+  productImage: any,
+  productLink: string
 }
 
 const Product = ({
   productTitle = 'My product',
-  price = '$20.00'
+  productLink = 'javascript:void(0)',
+  productImage = image
 }: Props) => {
   return (
     <section className={styles.product}>
-      <figure className={styles.imageWrap}>
-        <img className={styles.image} src={image} />
-      </figure>
-      <div className={styles.description}>
-        <h4>{productTitle}</h4>
-        <p>{price}</p>
-      </div>
+      <a className={styles.productLink} href={productLink} target='_blank'>
+        <figure className={styles.imageWrap}>
+          <img className={styles.image} src={productImage} />
+        </figure>
+        <div className={styles.description}>
+          <h4 className={styles.productTitle}>{productTitle}</h4>
+        </div>
+      </a>
     </section>
   )
 }
@@ -33,20 +40,24 @@ class FeaturedProduct extends React.Component {
         <section className={styles.content}>
           <h3 className={styles.header}>Shop</h3>
           <Product
-            productTitle={'First Product'}
-            price={'$24.00'}
+            productTitle={'Muse'}
+            productLink='https://shop.thriveglobal.com/products/muse-the-brain-sensing-headband'
+            productImage={image}
           />
           <Product
-            productTitle={'Second Product'}
-            price={'$24.00'}
+            productTitle={'Sense'}
+            productLink='https://shop.thriveglobal.com/products/sense-with-voice'
+            productImage={image2}
           />
           <Product
-            productTitle={'Third Product'}
-            price={'$24.00'}
+            productTitle={'Thrive Pillow'}
+            productLink='https://shop.thriveglobal.com/products/the-thrive-pillow-by-hill-house-home'
+            productImage={image3}
           />
           <Product
-            productTitle={'Fourth Product'}
-            price={'$24.00'}
+            productTitle={'Phone Bed'}
+            productLink='https://shop.thriveglobal.com/products/the-thrive-pillow-by-hill-house-home'
+            productImage={image4}
           />
         </section>
       </section>
