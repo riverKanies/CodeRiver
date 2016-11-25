f5f2f73fe556b7c7c0c6850dfc23c210628b2ffc
Andrew Fader <fader@yagni.co>
2016-11-25 02:56:27 -0500

    First stab at facebook oauth

--- universal/components/LoginForm/component.js
+++ universal/components/LoginForm/component.js
@@ -4,6 +4,8 @@ import { Field } from 'redux-form'
 import Button from 'components/Button'
 import Input from 'components/Input'
 import GenericFormError from 'components/GenericFormError'
+import FacebookLogin from 'react-facebook-login'
+import { httpPost } from 'lib/http'
 import styles from './styles'
 
 type renderSubmitField = {
@@ -27,15 +29,33 @@ const renderSubmit = ({ valid = false, submitting = false, handleSubmit }: rende
 type signupProps = {
   renderField: string,
   handleSubmit: Function,
+  handleFacebook: Function,
   submitting: boolean,
   valid: boolean,
 };
 
+const responseFacebook = (response) => {
+  return httpPost('/api/auth/facebook/callback', response).then(result => {
+    window.handleFacebook(result)
+  });
+}
+
 const LoginForm = (props: signupProps) => {
+  window.handleFacebook = props.handleFacebook
   return (
     <section className={styles.container}>
       <div className={styles.headerRow}>
         <h2>Log In</h2>
+      </div>
+      <div className={styles.headerRow}>
+        <FacebookLogin
+          appId='232613377159246'
+          autoLoad={true}
+          fields="email"
+          callback={responseFacebook}
+          cssClass="my-facebook-button-class"
+          icon="fa-facebook"
+        />
       </div>
       <form>
         <GenericFormError {...props} />
--- universal/components/SignupForm/component.js
+++ universal/components/SignupForm/component.js
@@ -5,6 +5,8 @@ import GenericFormError from 'components/GenericFormError'
 import Button from 'components/Button'
 import Input from 'components/Input'
 import styles from './styles'
+import FacebookLogin from 'react-facebook-login'
+import { httpPost } from 'lib/http'
 
 type renderSubmitField = {
   valid: boolean,
@@ -27,16 +29,34 @@ const renderSubmit = ({ valid = false, submitting = false, handleSubmit }: rende
 type signupProps = {
   renderField: string,
   handleSubmit: Function,
+  handleFacebook: Function,
   submitting: boolean,
   valid: boolean,
   message: string
 };
 
+const responseFacebook = (response) => {
+  return httpPost('/api/auth/facebook/callback', response).then(result => {
+    window.handleFacebook(result)
+  });
+}
+
 const SignUpForm = (props: signupProps) => {
+  window.handleFacebook = props.handleFacebook
   return (
     <section className={styles.container}>
-      <div className={styles.headerRow}>
+     <div className={styles.headerRow}>
         <h2>Sign up to start thriving</h2>
+      </div>
+      <div className={styles.headerRow}>
+        <FacebookLogin
+          appId='232613377159246'
+          autoLoad={true}
+          fields="email"
+          callback={responseFacebook}
+          cssClass="my-facebook-button-class"
+          icon="fa-facebook"
+        />
       </div>
       <form>
         <GenericFormError {...props} />
