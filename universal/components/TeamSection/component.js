3c4cf9efde7a3a0f6f94df7a34b49b0df9ab4e99
Omar Niazi <ramoizain@gmail.com>
2016-11-26 13:02:29 -0500

    Got modal showing teammember content

--- universal/components/TeamSection/component.js
+++ universal/components/TeamSection/component.js
@@ -22,7 +22,8 @@ export class TeamSection extends React.Component {
     super(props);
     this.state = {
       teamMembers: [],
-      showModal: false
+      showModal: false,
+      modalContent: <div></div>
     }
   }
 
@@ -37,9 +38,16 @@ export class TeamSection extends React.Component {
     })
   }
 
-  handleClick() {
+  handleClick(member) {
     this.setState({
-      showModal: !this.state.showModal
+      showModal: !this.state.showModal,
+      modalContent: (
+        <div>
+          <h3> {member.name} </h3>
+          <h4> {member.title} </h4>
+          <p> {member.bio} </p>
+        </div>
+      )
     })
   }
 
@@ -48,11 +56,11 @@ export class TeamSection extends React.Component {
       <section className={styles.SectionContainer}>
         <section className={styles.row} >
           <h2 className={styles.title}> Our Team </h2>
-          <Modal show={this.state.showModal} onClick={this.handleClick.bind(this)}/>
+          <Modal show={this.state.showModal} onClick={this.handleClick.bind(this)} content={this.state.modalContent} />
           <FounderBio />
           <section className={styles.biosRow}>
             {this.state.teamMembers.map(member =>
-              <TeamBio {...member} onClick={this.handleClick.bind(this)} key={member.name} />
+              <TeamBio {...member} onClick={()=>this.handleClick.call(this, member)} key={member.name} />
             )}
           </section>
         </section>
