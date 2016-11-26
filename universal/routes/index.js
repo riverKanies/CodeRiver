e2c14c3ded2f5e8110d3b231075e21583a2e373b
Omar Niazi <ramoizain@gmail.com>
2016-11-26 15:02:50 -0500

    Started popupstore landing page

--- universal/routes/index.js
+++ universal/routes/index.js
@@ -11,6 +11,7 @@ import Apps from 'layouts/Apps'
 import Thrive from 'layouts/Thrive'
 import Pulse from 'layouts/Pulse'
 import Interests from 'layouts/Interests'
+import PopupStore from 'layouts/PopupStore'
 import PulseScore from 'containers/PulseScore'
 import GlassDoor from 'layouts/GlassDoor'
 // import Engage from 'layouts/Engage'
@@ -83,6 +84,10 @@ export const createRoutes = (store) => ([
         path: 'corporate-pledge',
         component: GlassDoor
       },
+      {
+        path: 'popupstore',
+        component: PopupStore
+      },      
       {
         path: 'signup',
         component: SignUp
