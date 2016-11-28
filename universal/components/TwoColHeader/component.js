b23fe0298ecb7ee4996c922413b3db8ab24a607c
beauingfield <beau.wingfield@gmail.com>
2016-11-28 11:06:13 -0500

    reusing other component as foundation

---
+++ universal/components/TwoColHeader/component.js
@@ -0,0 +1,43 @@
+/* @flow */
+import React from 'react'
+import styles from './styles'
+
+type Props = {
+  bgColor: 'default' | 'transparent' | 'lightGray' | 'purple' | 'blue',
+  marginBottom: 'marginTrue' | 'marginFalse',
+  hasTitle: 'titleTrue' | 'titleFalse',
+  title: string,
+  colTextOne: any,
+  colTextTwo: any
+}
+
+const defaultText = ['Hello, I am some text!', 'I am also text!']
+
+const TwoColText = ({
+  bgColor = 'default',
+  marginBottom = 'marginTrue',
+  hasTitle = 'titleFalse',
+  title = 'TwoColText',
+  colTextOne = defaultText,
+  colTextTwo = defaultText
+}: Props) => {
+  return (
+    <section className={styles[bgColor][marginBottom]}>
+      <div className={styles.container}>
+        <header className={styles[hasTitle]}>
+          <h2>{title}</h2>
+        </header>
+        <section className={styles.columnWrap}>
+          <div className={styles.column}>
+            {colTextOne.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
+          </div>
+          <div className={styles.column}>
+            {colTextTwo.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
+          </div>
+        </section>
+      </div>
+    </section>
+  )
+}
+
+export default TwoColText
