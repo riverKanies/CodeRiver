4d8a3ac09bf0db217fb7b8d08959fa8035da3433
Andrew Frank <afrank@spartansystems.co>
2016-11-28 21:14:16 -0500

    fixed footer. was weird.

--- universal/layouts/Profile/component.js
+++ universal/layouts/Profile/component.js
@@ -1,23 +1,15 @@
 /* @flow */
 import React from 'react'
 import Profile from 'components/Profile'
-import FeaturedStory from 'components/FeaturedStory'
 
-const pulse = {
-  hasAuthor: 'authorFalse',
-  details: `Begin with the Thrive Pulse, our TK-question assessment that will
-    help you take stockof your life and identify areas of improvement.`,
-  title: 'What\'s My Thrive Pulse',
-  externalLink: {
-    linkTo: '/pulse'
-  }
-}
+// const pulse = {
+//
+// }
 
 export default function () {
   return (
     <div>
       <Profile />
-      <FeaturedStory {...pulse} />
     </div>
   )
 }
