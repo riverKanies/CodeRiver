0c216f974ac174a700281b435b7489fd0e2da2a5
beauingfield <beau.wingfield@gmail.com>
2016-11-29 13:31:00 -0500

    card styles

--- universal/components/PathwayCard/component.js
+++ universal/components/PathwayCard/component.js
@@ -1,7 +1,6 @@
 /* @flow */
 import React from 'react'
 import styles from './styles'
-import defaultImage from './assets/pathFPO.jpg'
 import { Link } from 'react-router'
 
 type Props = {
@@ -16,18 +15,13 @@ const defaultText = [`Pathway Title`]
 const PathwayCard = ({
   title = defaultText[0],
   description = 'Vestibulum id ligula porta felis euismod semper.',
-  image = defaultImage,
   id }: Props) => {
   return (
     <Link to={`/pathways/${id}`} className={styles.card}>
       <section className={styles.cardContainer}>
-        <span className={styles.imageWrap}>
-          <img className={styles.image} src={image} />
-        </span>
-        <section className={styles.description}>
-          <h3 className={styles.header}>{title}</h3>
-          <p className={styles.synopsis}>{description}</p>
-        </section>
+        <h3 className={styles.header}>{title}</h3>
+        <div className={styles.divider} />
+        <p className={styles.synopsis}>{description}</p>
       </section>
     </Link>
   )
