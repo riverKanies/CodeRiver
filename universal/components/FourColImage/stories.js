974a012a01e76d36ffc6997d17190eda263e7e90
Andrew Frank <afrank@spartansystems.co>
2016-11-29 15:21:03 -0500

    story done. ready to merge

--- universal/components/FourColImage/stories.js
+++ universal/components/FourColImage/stories.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { WithNotes } from '@kadira/storybook-addon-notes'
-import { withKnobs, text } from '@kadira/storybook-addon-knobs'
+import { withKnobs, select } from '@kadira/storybook-addon-knobs'
 
 import FourColImage from './component'
 
 const notes = 'This story demonstrates the props that can be passed to FourColImage'
 
 const props = {
-  title: 'FourColImage Title'
+  bgColor: [
+    'default',
+    'white',
+    'blue',
+    'gray',
+    'fade'
+  ],
+  gutters: [
+    'guttersTrue',
+    'guttersFalse'
+  ]
 }
 
 storiesOf('FourColImage', module)
@@ -17,7 +27,8 @@ storiesOf('FourColImage', module)
     return (
       <WithNotes notes={notes}>
         <FourColImage
-          title={text('Title', props.title)}
+          bgColor={select('Section BG Color', props.bgColor, props.bgColor[0])}
+          gutters={select('Want gutters between images?', props.gutters, props.gutters[0])}
         />
       </WithNotes>
     )
