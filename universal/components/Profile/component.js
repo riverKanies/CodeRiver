90fd637cdcdc6fce5308a774d99bc65c0caf356c
Andrew Frank <afrank@spartansystems.co>
2016-11-28 20:14:51 -0500

    first style commit

--- universal/components/Profile/component.js
+++ universal/components/Profile/component.js
@@ -10,15 +10,15 @@ type Props = {
 
 const Profile = ({
   email,
-  userName,
+  userName = 'Name',
   logOutAction
 }: Props) => {
   if (!email) return null
   return (
     <section className={styles.profile}>
       <header className={styles.greeting}>
-        <h1>Welcome, {userName}</h1>
-        <h4>Here you can edit your account settings</h4>
+        <h1 className={styles.h1}>Welcome, {userName}</h1>
+        <h4 className={styles.h4}>Here you can edit your account settings</h4>
       </header>
       <section className={styles.myAccount}>
         <div className={styles.container}>
@@ -30,26 +30,26 @@ const Profile = ({
           <form className={styles.myProfile}>
             <fieldset className={styles.accountAbout}>
               <div className={styles.inputWrap}>
-                <label>Name</label>
-                <input type='text' name='name' value='name' placeholder='Name' />
+                <label className={styles.label}>Name</label>
+                <input className={styles.input} type='text' name='name' placeholder='Name' />
               </div>
               <div className={styles.inputWrap}>
-                <label>Email</label>
-                <input type='email' name='email' value='email' placeholder='Email' />
+                <label className={styles.label}>Email</label>
+                <input className={styles.input} type='email' name='email' placeholder='Email' />
               </div>
             </fieldset>
             <fieldset className={styles.accountPreferences}>
               <div className={styles.inputWrap}>
-                <input type='checkbox' name='newsletter' value='newsletter' />
-                <label>Newsletter</label>
+                <input className={styles.check} type='checkbox' name='newsletter' />
+                <label className={styles.label}>Newsletter</label>
               </div>
               <div className={styles.inputWrap}>
-                <input type='checkbox' name='emailsThrive' value='emailsThrive' />
-                <label>Send me occasional emails from Thrive Global</label>
+                <input className={styles.check} type='checkbox' name='emailsThrive' />
+                <label className={styles.label}>Send me occasional emails from Thrive Global</label>
               </div>
             </fieldset>
             <fieldset className={styles.save}>
-              <input type='submit' value='Save' />
+              <input className={styles.input} type='submit' />
             </fieldset>
           </form>
 
@@ -60,20 +60,20 @@ const Profile = ({
           <form className={styles.changePassword}>
             <fieldset className={styles.password}>
               <div className={styles.inputWrap}>
-                <label>Old Password</label>
-                <input type='text' name='oldPassword' value='oldPassword' placeholder='Old Password' />
+                <label className={styles.label}>Old Password</label>
+                <input className={styles.input} type='text' name='oldPassword' placeholder='Old Password' />
               </div>
               <div className={styles.inputWrap}>
-                <label>New Password</label>
-                <input type='text' name='newPassword' value='newPassword' placeholder='New Password' />
+                <label className={styles.label}>New Password</label>
+                <input className={styles.input} type='text' name='newPassword' placeholder='New Password' />
               </div>
               <div className={styles.inputWrap}>
-                <label>Confirm Password</label>
-                <input type='text' name='confirmPassword' value='confirmPassword' placeholder='Confirm Password' />
+                <label className={styles.label}>Confirm Password</label>
+                <input className={styles.input} type='text' name='confirmPassword' placeholder='Confirm Password' />
               </div>
             </fieldset>
             <fieldset className={styles.save}>
-              <input type='submit' value='Change' />
+              <input className={styles.input} type='submit' value='Change' />
             </fieldset>
           </form>
 
