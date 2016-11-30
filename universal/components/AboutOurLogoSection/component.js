c30786484052269c28ff96741b6911424fdcf18e
Dossey Richards <dosseyrichards@Dosseys-MacBook.local>
2016-11-29 20:54:11 -0500

    logo change

--- universal/components/AboutOurLogoSection/component.js
+++ universal/components/AboutOurLogoSection/component.js
@@ -4,7 +4,7 @@ import FullWidthTwoCol from 'components/FullWidthTwoCol'
 const title ="About Our Logo"
 const text = ["Interested in bringing Thrive trainings to your workplace or nonprofit organization? Email our director of trainings Joey Hubbard at jh@thriveglobal.com."]
 const label = {"label":"Thrive Logo"}
-import imageSrc from "./assets/logo.png"
+import imageSrc from "./assets/logo.jpg"
 export const AboutOurLogoSection = () => {
     return (
     <section className={styles.SectionContainer}>
