dc50e074ffbf91b237ddf4139d539b7166596e59
Andrew Frank <afrank@spartansystems.co>
2016-12-07 10:46:36 -0500

    button in. styling modal for rwd.

--- universal/components/Modal/component.js
+++ universal/components/Modal/component.js
@@ -20,13 +20,22 @@ const Modal = ({
   })
 
   return (
-    <div className={classes} onClick={onClick}>
+    <section className={classes} onClick={onClick}>
       <div className={styles.middleWrapper}>
-        <div className={styles.modalContent}>
+        <section className={styles.modalContent}>
           {content}
-        </div>
+          <nav className={styles.closeModal}>
+            <svg className={styles.closeButton} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 40 40'>
+              <title>Close Modal</title>
+              <path className={styles.closeBar}
+                d='M27.16,29.81l-17-17a1.88,1.88,0,0,1,2.65-2.65l17,17a1.88,1.88,0,0,1-2.65,2.65Z' />
+              <path className={styles.closeBar}
+                d='M29.81,12.84l-17,17a1.88,1.88,0,0,1-2.65-2.65l17-17a1.88,1.88,0,0,1,2.65,2.65Z' />
+            </svg>
+          </nav>
+        </section>
       </div>
-    </div>
+    </section>
   )
 }
 
