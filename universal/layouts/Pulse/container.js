95f1676e2bbec52df0dcc307eaa7656fa9d6df4f
Omar Niazi <omar@thriveglobal.com>
2016-12-09 11:14:42 -0500

    Removed login/signup requirement for pulse survey

--- universal/layouts/Pulse/container.js
+++ universal/layouts/Pulse/container.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import { connect } from 'react-redux'
-
-import Assessments from 'components/Assessments'
-import { assessmentsSelector } from 'modules/UserSession/selectors'
-
-type Props = {
-  dispatch: Function,
-  renderLoginForm: boolean,
-  requestPending: boolean,
-  creds: Object
-}
+import TypeForm from 'components/TypeForm'
 
 export class PulseLayout extends React.Component {
-  props: Props
-
   render () {
-    return <Assessments {...this.props} typeformKey='sKzAso' />
+    return <TypeForm typeformkey='sKzAso' />
   }
 }
 
-export default connect(assessmentsSelector)(PulseLayout)
+export default PulseLayout
