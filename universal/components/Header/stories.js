87d6fd1da796862eca0b54587635c736abc2bacf
Jonathan Barber <jbarber@spartansystems.co>
2016-12-13 14:25:51 -0500

    header story fix

--- universal/components/Header/stories.js
+++ universal/components/Header/stories.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { WithNotes } from '@kadira/storybook-addon-notes'
+import { withKnobs } from '@kadira/storybook-addon-knobs'
+import { WithProvider } from 'lib/testHelpers'
 
 import Header from './component'
 
+const notes = 'This story demonstrates the props that can be passed to Header'
+
 storiesOf('Header', module)
-  .add('header component', () => {
+  .addDecorator(withKnobs)
+  .add('with props', () => {
     return (
-      <WithNotes notes={'This story demonstrates the props that can be passed to <Button />'}>
-        <Header />
+      <WithNotes notes={notes}>
+        <WithProvider>
+          <Header />
+        </WithProvider>
       </WithNotes>
     )
   })
