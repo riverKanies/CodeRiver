871028c17a487cf3c02ae7f6258922feaf9ee1cd
Andrew Frank <afrank@spartansystems.co>
2016-12-13 13:24:58 -0500

    refactored markup. still need some tlc.

--- universal/layouts/Grow/component.js
+++ universal/layouts/Grow/component.js
@@ -9,7 +9,6 @@ import TwoColTextImage from 'components/TwoColTextImage'
 import PathwaySection from 'components/PathwaySection'
 import QuoteSection from 'components/QuoteSection'
 import VerticalTimeline from 'components/VerticalTimeline'
-import VerticalTimelineItem from 'components/VerticalTimeline/VerticalTimelineItem'
 
 // Content Import
 import kobeImage from './assets/kobe.jpg'
@@ -61,39 +60,6 @@ const eCourse = {
     linkStyle: 'violet'
   }
 }
-const timelinePathways = {
-  itemPosition: 'itemLeft',
-  title: 'Pathways',
-  sectionText: [
-    `There are five pathways built of multiple microsteps you can practice
-     in a sustainable way. The steps are small and very easy to integrate into your life,
-     but the impact is transformational. This is the part where we go from knowing
-     what to do to actually doing it. Choose the Pathway you want to begin with--
-     Joy, Calm, Productivity, Well-Being and Purpose.`
-  ]
-}
-const timelineJourney = {
-  itemPosition: 'itemRight',
-  title: 'Journeys',
-  sectionText: [
-    `The purpose of a microstep, simply stated, is the tiniest step you can
-    take to put you on the path towards what you want to achieve. Once you are on
-    the path, myriad opportunities open up for you to improve your health, productivity,
-    and happiness. Our microsteps are practical and they work! Pick the microstep
-    that becomes your cornerstone habit, and keep adding in the order that works best for you.`
-  ]
-}
-const timelineMicrostep = {
-  itemPosition: 'itemLeft',
-  title: 'Microsteps',
-  sectionText: [
-    `There are five Pathway Protocols -- experiment with them, discover what most resonates with you,
-     and return to it again and again. You can do these at any time -- as you begin your pathways,
-     on your commute, during a break in your day, or before you go to bed at night. We have bracketed
-     in italics commentary to help you practice the protocols the first few times, after
-     which it will become second nature.`
-  ]
-}
 const quote = {
   bgColor: 'pattern',
   quoteImage: ariannaQuote,
@@ -110,11 +76,7 @@ export const GrowView = () => (
       <TextBanner {...banner} />
       <CTASection {...linkToAssessment} />
       <PathwaySection />
-      <VerticalTimeline>
-        <VerticalTimelineItem {...timelinePathways} />
-        <VerticalTimelineItem {...timelineJourney} />
-        <VerticalTimelineItem {...timelineMicrostep} />
-      </VerticalTimeline>
+      <VerticalTimeline />
       <TwoColTextImage {...eCourse} />
       <QuoteSection {...quote} />
     </section>
