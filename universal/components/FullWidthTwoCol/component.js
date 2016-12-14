d0233b86d3778127f4ee990fc5cd3f1430e0c84b
Andrew Frank <afrank@spartansystems.co>
2016-12-14 11:00:08 -0500

    lots of tests added.

--- universal/components/FullWidthTwoCol/component.js
+++ universal/components/FullWidthTwoCol/component.js
@@ -4,7 +4,13 @@ import styles from './styles'
 import dummyImage from './assets/thriveScience.jpg'
 import ContentLabel from 'components/ContentLabel'
 import ExternalLink from 'components/ExternalLink'
-import Button from 'components/Button'
+
+type LinkProps = {
+  linkText: string,
+  linkTo: string,
+  linkStyle: string,
+  target: string
+}
 
 type Props = {
   sectionClass: 'default' | 'lightGray' | 'fade',
@@ -15,18 +21,17 @@ type Props = {
   image: string,
   button: any,
   hasButton: boolean,
-  linktoURL: string,
-  link: Object
+  link: LinkProps
 };
 
 const defaultText = [`Morbi leo risus, porta ac consectetur ac, vestibulum at
-eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
-mattis consectetur purus sit amet fermentum.`]
+  eros. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras
+  mattis consectetur purus sit amet fermentum.`]
 
-// True/False Logic For Label
+  // True/False Logic For Label
 function renderLabel ({
-  hasLabel,
-  label
+    hasLabel,
+    label
   }: { hasLabel: boolean, label: Object}) {
   if (hasLabel) {
     return (
@@ -35,15 +40,14 @@ function renderLabel ({
       </span>
     )
   }
-  return (null)
+  return null
 }
 
 // True/False Logic For Button/Link
 function renderButton ({
-  hasButton,
-  linktoURL,
-  link
-  }: { hasButton: boolean, linktoURL: string, link: Object}) {
+    hasButton,
+    link
+  }: { hasButton: boolean, link: Object}) {
   if (hasButton) {
     return (
       <span className={styles.button}>
@@ -54,20 +58,25 @@ function renderButton ({
   return (null)
 }
 
-// True/False Logic For Wrapping Image In Anchor
 function renderImage ({
-  image,
-  title,
   hasButton,
-  linktoURL
-  }: { image: string, title: string, hasButton: boolean, linktoURL: string }) {
-  if (hasButton) {
+  link,
+  title,
+  image
+}: { hasButton: boolean, link: LinkProps, image: string, title: string }) {
+  if (!hasButton || !link) {
     return (
-      <a className={styles.imgLink} href={linktoURL}><img className={styles.image} src={image} alt={title} /></a>
+      <figure className={styles.imageWrap}>
+        <img className={styles.image} src={image} alt={title} />
+      </figure>
     )
   }
   return (
-    <img className={styles.image} src={image} alt={title} />
+    <figure className={styles.imageWrap}>
+      <a href={link.linkTo} target={link.target || '_blank'} >
+        <img className={styles.image} src={image} alt={title} />
+      </a>
+    </figure>
   )
 }
 
@@ -78,32 +87,26 @@ const FullWidthTwoCol = ({
   label = {},
   sectionText = defaultText,
   image = dummyImage,
-  button = null,
   hasButton = true,
-  linktoURL = 'http://www.thriveglobal.com',
   link = {}
-
 }: Props) => {
   return (
     <section className={styles[sectionClass]}>
-      <section className={styles.container}>
+      <div className={styles.container}>
         <div className={styles.column}>
-          <figure className={styles.imageWrap}>
-            {renderImage({image, title, hasButton, linktoURL})}
-          </figure>
+          {renderImage({ hasButton, image, title, link })}
         </div>
         <div className={styles.column}>
-          <section className={styles.sectionContent}>
+          <div className={styles.sectionContent}>
             {renderLabel({hasLabel, label})}
             <header className={styles.header}>
               <h2>{title}</h2>
             </header>
             {sectionText.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
-            {button ? <Button {...button} /> : ''}
             {renderButton({hasButton, link})}
-          </section>
+          </div>
         </div>
-      </section>
+      </div>
     </section>
   )
 }
--- universal/components/FullWidthTwoCol/component.spec.js
+++ universal/components/FullWidthTwoCol/component.spec.js
@@ -1,8 +1,96 @@
 import React from 'react'
 import FullWidthTwoCol from './component'
+import { shallow } from 'enzyme'
 
 describe('(Component) FullWidthTwoCol', () => {
-  it('should exist', () => {
+  context('with default props', () => {
+    const subject = shallow(<FullWidthTwoCol />)
 
+    it('should render ok with the default props', () => {
+      const target = subject.find('section')
+
+      expect(target.length).toEqual(1)
+    })
+
+    it('renders a default image', () => {
+      const target = subject.find('img')
+
+      expect(target.length).toEqual(1)
+    })
+
+    it('renders a default title', () => {
+      const target = subject.find('h2')
+
+      expect(target.length).toEqual(1)
+      expect(target.text()).toEqual('Title')
+    })
+
+    it('has a ContentLabel', () => {
+      const target = subject.find('ContentLabel')
+
+      expect(target.length).toEqual(1)
+    })
+
+    it('renders one paragraph of section text', () => {
+      const target = subject.find('p')
+
+      expect(target.length).toEqual(1)
+    })
+
+    it('renders an external link by default', () => {
+      const target = subject.find('ExternalLink')
+
+      expect(target.length).toEqual(1)
+    })
+  })
+
+  context('variable section text', () => {
+    const sectionText = ['hi', 'hello', 'goodbye']
+    const subject = shallow(<FullWidthTwoCol sectionText={sectionText} />)
+
+    it('renders no button', () => {
+      const target = subject.find('p')
+
+      expect(target.length).toEqual(3)
+    })
+  })
+
+  context('hasButton false', () => {
+    const subject = shallow(<FullWidthTwoCol hasButton={false} />)
+
+    it('renders no button', () => {
+      const target = subject.find('ExternalLink')
+
+      expect(target.length).toEqual(0)
+    })
+  })
+
+  context('rendering images with and without links', () => {
+    const defaultProps = {
+      link: {
+        linkText: 'Read More',
+        linkTo: 'http://www.thriveglobal.com',
+        linkStyle: 'blue'
+      }
+    }
+    it('has no link when hasButton is false', () => {
+      const subject = shallow(
+        <FullWidthTwoCol {...defaultProps} hasButton={false} />
+      )
+      const target = subject.find('figure')
+      const link = target.find('a')
+
+      expect(link.length).toEqual(0)
+    })
+
+    it('has a link when hasButton is true', () => {
+      const subject = shallow(
+        <FullWidthTwoCol {...defaultProps} hasButton={true} />
+      )
+      const target = subject.find('figure')
+      const link = target.find('a')
+
+      expect(link.length).toEqual(1)
+    })
   })
 })
