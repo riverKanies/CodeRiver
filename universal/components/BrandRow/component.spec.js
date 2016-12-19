ee6ac24f8e8380aa7f354e6eacf7b51f69953571
Bill Brock <bbrock@spartansystems.co>
2016-12-19 16:27:46 -0500

    Make brand row dynamic (#356)
    _Why_?
    
    it was all hardcoded from launch
    and we needed to be able to use links and strings
    
    _How_?
    
    add tests
    allow the component to take in an array of logos
    
    _Side Effects_?
    
    naw

--- universal/components/BrandRow/component.spec.js
+++ universal/components/BrandRow/component.spec.js
@@ -1,8 +1,75 @@
 import React from 'react'
 import BrandRow from './component'
+import { shallow, mount } from 'enzyme'
+
+import logo_one from './assets/logo_one.jpg'
 
 describe('(Component) BrandRow', () => {
-  it('should exist', () => {
+  context('default case', () => {
+    it('renders the standard logo bars', () => {
+      const subject = shallow(<BrandRow />)
 
+      const target = subject.find('Logo')
+      expect(target.length).toEqual(5)
+    })
+
+    it('has no links', () => {
+      const subject = mount(<BrandRow />)
+
+      const target = subject.find('a')
+      expect(target.length).toEqual(0)
+    })
+  })
+
+  context('with images and links', () => {
+    const logos = [
+      { logoImage: logo_one, link: 'http://www.cats.com' },
+      { logoImage: logo_one, link: 'http://www.dogs.com' },
+      { logoImage: logo_one, link: 'http://www.hamsters.com' }
+    ]
+
+    it('has three logos', () => {
+      const subject = mount(<BrandRow logos={logos} />)
+
+      const target = subject.find('img')
+      expect(target.length).toEqual(3)
+    })
+
+    it('has three links', () => {
+      const subject = mount(<BrandRow logos={logos} />)
+
+      const target = subject.find('a')
+      expect(target.length).toEqual(3)
+    })
+
+    it('has the correct link on each logo', () => {
+      const subject = mount(<BrandRow logos={logos} />)
+
+      const target = subject.find('a')
+      target.forEach((t, i) => {
+        expect(t.props().href).toEqual(logos[i].link)
+      })
+    })
+  })
+
+  context('with images and no links', () => {
+    const logos = [
+      { logoImage: logo_one },
+      { logoImage: logo_one }
+    ]
+
+    it('has two logos', () => {
+      const subject = mount(<BrandRow logos={logos} />)
+
+      const target = subject.find('img')
+      expect(target.length).toEqual(2)
+    })
+
+    it('has no links', () => {
+      const subject = mount(<BrandRow logos={logos} />)
+
+      const target = subject.find('a')
+      expect(target.length).toEqual(0)
+    })
   })
 })
