2d69220b37b972045ea2bb9cc1c7e76640f26fed
Bill Brock <bbrock@spartansystems.co>
2016-12-20 12:56:42 -0500

    Updated foooter links as specified (#361)
    _Why_?
    
    so that our footer is standing on solid ground
    
    _How_?
    
    updated links
    created helper
    wrote specs
    
    _Side Effects_?
    no

--- universal/components/Footer/PageFooter.spec.js
+++ universal/components/Footer/PageFooter.spec.js
@@ -1,8 +1,84 @@
 import React from 'react'
 import Footer from './component'
+import { routesParser } from 'react-router-sitemap'
+import createRoutes from 'routes'
+import createStore from 'store/createStore'
+
+import { shallow } from 'enzyme'
 
 describe('(Component) Footer', () => {
-  it('should exist', () => {
-
+  const subject = shallow(<Footer />)
+  it('renders the footer element', () => {
+    expect(subject.find('footer').length).toEqual(1)
   })
+
+  context('footer links', () => {
+    const expectedLinks = [
+      { route: '/about', text: 'About Us' },
+      { route: '/who-we-are#careers', text: 'Careers' },
+      { route: '/who-we-are#contact', text: 'Contact' },
+      { route: '/about/corporate', text: 'Partnerships - Corporate' },
+      { route: '/about/content', text: 'Partnerships - Media' },
+      { route: '/about/commerce', text: 'Partnerships - Commerce' },
+      { route: '/popup', text: 'Pop-up Store' },
+      { route: '/press', text: 'Press' },
+      { route: '/team', text: 'Team' },
+    ]
+
+    const target = subject.find('#footerLinks')
+
+    function elemType (elem) {
+      return elem.find('Link').length > 0 ? 'Link' : 'a'
+    }
+
+    function getText (elem) {
+      if (elemType(elem) === 'a') {
+        return elem.find('a').text()
+      }
+      return elem.find('Link').children().first().text()
+    }
+
+    function getLinkLocation (elem) {
+      if (elemType(elem) === 'a') {
+        return elem.find('a').props().href
+      }
+      return elem.find('Link').props().to
+    }
+
+    it('renders the correct amount of links', () => {
+      expect(target.children().length).toEqual(expectedLinks.length)
+    })
+
+    it('has the correct link text for each link', () => {
+      target.children().forEach((link, idx) => {
+        expect(getText(link)).toEqual(expectedLinks[idx].text)
+      })
+    })
+
+    it('has the correct location for each link', () => {
+      target.children().forEach((link, idx) => {
+        expect(getLinkLocation(link)).toEqual(expectedLinks[idx].route)
+      })
+    })
+
+    it('renders external links if we need to use an anchor tag', () => {
+      target.children().forEach((link, idx) => {
+        const expectedElem = expectedLinks[idx].route.match('#') ? 'a' : 'Link'
+        expect(elemType(link)).toEqual(expectedElem)
+      })
+    })
+
+    it('has links that are all valid with react router', () => {
+      const store = createStore({})
+      const router = createRoutes(store)
+      const allRoutes = routesParser(router)
+
+      target.children().forEach((link) => {
+        if(elemType(link) === 'Link') {
+          expect(allRoutes).toContain(getLinkLocation(link))
+        }
+      })
+    })
+  })
+
 })
