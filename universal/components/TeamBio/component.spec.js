0c67669059f59e9de7df98f3edac87bede804fc6
Rob Lee <rob@nodeworks.com>
2016-12-22 14:09:54 -0500

    Component spec for TeamBio (#382)
    * component spec for TeamBio
    
    * removed mount from the import

--- universal/components/TeamBio/component.spec.js
+++ universal/components/TeamBio/component.spec.js
@@ -1,8 +1,38 @@
 import React from 'react'
 import TeamBio from './component'
+import { shallow } from 'enzyme'
+import imagePlaceholder from './assets/team-placeholder.jpg'
 
 describe('(Component) TeamBio', () => {
-  it('should exist', () => {
+  context('using default props', () => {
+    const subject = shallow(<TeamBio />)
 
+    it('should exist', () => {
+      const target = subject.find('h3')
+
+      expect(target.length).toEqual(1)
+    })
+  })
+
+  context('when passing in our own properties', () => {
+    const teamBioProps = {
+      title: 'Test title',
+      name: 'Test name',
+      image: imagePlaceholder
+    }
+
+    const subject = shallow(<TeamBio {...teamBioProps} />)
+
+    it('renders component with title', () => {
+      const target = subject.find('h4')
+
+      expect(target.text()).toEqual(teamBioProps.title)
+    })
+
+    it('renders component with name', () => {
+      const target = subject.find('h3')
+
+      expect(target.text()).toEqual(teamBioProps.name)
+    })
   })
 })
