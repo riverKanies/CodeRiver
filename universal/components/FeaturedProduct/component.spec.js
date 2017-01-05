40d5365ce14d28af56b44958a0709b2ca351384a
Rob Lee <rob@nodeworks.com>
2017-01-05 12:26:13 -0500

    FeaturedProduct spec

--- universal/components/FeaturedProduct/component.spec.js
+++ universal/components/FeaturedProduct/component.spec.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import FeaturedProduct from './component'
+import { shallow, mount } from 'enzyme'
 
 describe('(Component) FeaturedProduct', () => {
-  it('should exist', () => {
+  context('default case', () => {
+    const subject = shallow(<FeaturedProduct />)
 
+    it('should exist', () => {
+      const target = subject.find('h3')
+      expect(target.length).toEqual(1)
+    })
+  })
+
+  context('using defined parameters', () => {
+    const subject = mount(<FeaturedProduct />)
+
+    it('should have 3 products', () => {
+      const target = subject.find('figure')
+
+      expect(target.length).toEqual(3)
+    })
   })
 })
