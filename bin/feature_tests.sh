27ce374fb53a949cdd6d3d7efd2c205c8faf2ea6
Omar Niazi <omar@thriveglobal.com>
2017-01-06 19:08:54 -0500

    Small change

--- bin/feature_tests.sh
+++ bin/feature_tests.sh
@@ -2,6 +2,6 @@
 
 nohup npm run start:test &
 nohup java -jar bin/selenium-server-standalone-3.0.1.jar &
-../nightwatch/wait_for_server.sh
+./nightwatch/wait_for_server.sh
 npm run test:circle:features
-cat ../coverage/lcov.info | ../node_modules/.bin/codacy-coverage
+cat ./coverage/lcov.info | ./node_modules/.bin/codacy-coverage
