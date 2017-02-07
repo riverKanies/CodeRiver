c50984fb17c421fc7108a079420941d238fa7e2b
River Kanies <river.kanies@gmail.com>
2017-02-07 11:19:53 -0500

    rc-slider

--- universal/components/Quoter/component.js
+++ universal/components/Quoter/component.js
@@ -3,36 +3,61 @@ import React from 'react'
 import QuoterForm from 'components/QuoterForm'
 import styles from './styles'
 
+import './sliderStyles.css'
+import Slider from 'rc-slider'
+
 type Props = {
 }
 
-const Quoter = (props) => {
-  return (
-    <section className={styles.profile}>
+class Quoter extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {}
+    this.state.coverage = 500000
+
+    this.updateCoverage = this.updateCoverage.bind(this)
+  }
+
+  render () {
+    return (<section className={styles.profile}>
       <section className={styles.myAccount}>
         <div className={styles.container}>
-          {renderQuotes(props.quotes)}
+          {this.renderQuotes(this.props.quotes)}
           <header className={styles.accountHeader}>
             <h3>Get a Quote</h3>
           </header>
-          <QuoterForm quoterErrors={props.errors}/>
+          <QuoterForm quoterErrors={this.props.errors}/>
         </div>
       </section>
-    </section>
-  )
+    </section>)
+  }
+  renderQuotes(quotes) {
+    //if (quotes.length < 1) return ''
+    return (<div>
+      <div style={{ width: '100%' }}>
+        <p>Coverage: ${numberWithCommas(this.state.coverage)}</p>
+        <Slider min={100000} max={1000000} value={this.state.coverage} step={100000} onChange={this.updateCoverage}/>
+      </div>
+      <ul>
+        {quotes.map((q,i)=>{
+          return <li key={i}>
+            <img src={q.logo_url} />
+            <p>Company: {q.company_name}</p>
+            <p>Monthly Premium: ${q.premium_monthly}</p>
+            <br/>
+          </li>
+        })}
+      </ul>
+    </div>)
+  }
+  updateCoverage (coverage) {
+    this.setState({coverage})
+  }
 }
 
-function renderQuotes(quotes) {
-  return (<ul>
-    {quotes.map((q,i)=>{
-      return <li key={i}>
-        <img src={q.logo_url} />
-        <p>Company: {q.company_name}</p>
-        <p>Monthly Premium: ${q.premium_monthly}</p>
-        <br/>
-      </li>
-    })}
-  </ul>)
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 export default Quoter
