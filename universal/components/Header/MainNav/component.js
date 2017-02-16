16aac126977c7d2172cf2129669353464d4c849b
River Kanies <river.kanies@gmail.com>
2017-02-16 12:23:26 -0500

    dropdown test button

--- universal/components/Header/MainNav/component.js
+++ universal/components/Header/MainNav/component.js
@@ -11,9 +11,22 @@ class MainNav extends React.Component {
   constructor () {
     super(...arguments)
 
+    this.state = {}
+    this.state.dropped = false
+
+    this.drop = this.drop.bind(this)
+  }
+
+  componentDidUpdate () {
+    const dd = document.getElementById('dropdown')
+    if (dd) {
+      console.log('add',)
+      dd.classList.add(styles.dropdownContent)
+    }
   }
 
   render () {
+    console.log('drop', this.state.dropped)
     return (
       <nav role='navigation' className={styles.mainNav}>
         <Link onClick={this.props.hideMenu}
@@ -30,8 +43,25 @@ class MainNav extends React.Component {
           activeClassName={styles.activeRoute}>
           Home
         </Link>
+        <div className={styles.dropdown}>
+          <button onClick={this.drop}>Drop It</button>
+          {this.renderDropdown()}
+        </div>
       </nav>
     )
+  }
+
+  drop () {
+    this.setState({dropped: !this.state.dropped})
+  }
+
+  renderDropdown () {
+    if (!this.state.dropped) return ''
+    return (<div id='dropdown'>
+      <p>
+        HomeDrop
+      </p>
+    </div>)
   }
 }
 
