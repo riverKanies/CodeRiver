3517e8ab79852d6dc7611a4b24bb8dca556a7c3e
River Kanies <river.kanies@gmail.com>
2017-02-23 12:36:48 -0500

    update footer

--- universal/components/Footer/component.js
+++ universal/components/Footer/component.js
@@ -7,27 +7,16 @@ export class Footer extends React.Component {
   render () {
     return (<div className={styles.footer}>
       <section className={styles.section}>
-        <h3>Footer Section 1</h3>
-        <Link id='test1' to='/test1' className={styles.link}>testicular1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
+        <h3>More on PlanMade</h3>
+        <Link id='about' to='/about' className={styles.link}>About</Link>
+        <Link id='privacy' to='/privacy' className={styles.link}>Privacy</Link>
+        <Link id='terms' to='/terms' className={styles.link}>Terms</Link>
+        <Link id='press' to='/press' className={styles.link}>Press</Link>
       </section>
       <section className={styles.section}>
-        <h3>Footer Section 2</h3>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>testastic1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-      </section>
-      <section className={styles.section}>
-        <h3>Footer Section 2</h3>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
-        <Link id='test1' to='/test1' className={styles.link}>test1</Link>
+        <h3>Resources</h3>
+        <Link id='licences' to='/licences' className={styles.link}>Licences</Link>
+        <Link id='blog' to='/blog' className={styles.link}>blog</Link>
       </section>
     </div>)
   }
