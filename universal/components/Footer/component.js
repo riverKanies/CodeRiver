73cd008c8575e026b9703e97b53bc4e3b6f48f2b
River Kanies <river.kanies@gmail.com>
2017-05-24 13:40:03 -0400

    footer and colors

--- universal/components/Footer/component.js
+++ universal/components/Footer/component.js
@@ -7,16 +7,13 @@ export class Footer extends React.Component {
   render () {
     return (<div className={styles.footer}>
       <section className={styles.section}>
-        <h3>More on PlanMade</h3>
+        <h3>More on CodeRiver</h3>
         <Link id='about' to='/about' className={styles.link}>About</Link>
-        <Link id='privacy' to='/privacy' className={styles.link}>Privacy</Link>
-        <Link id='terms' to='/terms' className={styles.link}>Terms</Link>
-        <Link id='press' to='/press' className={styles.link}>Press</Link>
       </section>
       <section className={styles.section}>
-        <h3>Resources</h3>
-        <Link id='licenses' to='/licenses' className={styles.link}>Licenses</Link>
-        <Link id='blog' to='/blog' className={styles.link}>blog</Link>
+        <h3>Connect</h3>
+        <a id='linkedin' href='https://www.linkedin.com/in/river-kanies-95706389' className={styles.link}>LinkedIn</a>
+        <a id='github' href='https://github.com/riverKanies' className={styles.link}>GitHub</a>
       </section>
     </div>)
   }
