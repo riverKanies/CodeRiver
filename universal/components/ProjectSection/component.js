b7c9d304773182f036342c0d88445f07b30efe77
River Kanies <river.kanies@gmail.com>
2017-05-26 15:49:45 -0400

    device graphics for portfolio

--- universal/components/ProjectSection/component.js
+++ universal/components/ProjectSection/component.js
@@ -5,14 +5,31 @@ import styles from './styles'
 
 class ProjectSection extends React.Component {
   render () {
-    return (<div>
-      <h2 className={styles.header}>{this.props.header}</h2>
-      <div className={styles.imageContainer}>
-        <a href={this.props.link} target="_blank">
-          <img className={styles.image} src={this.props.image} />
-        </a>
+    return (<div className={styles.container}>
+      <div className={styles.graphicsContainer}>
+        <div className={styles.computerSection}>
+          <div className={styles.imageContainer}>
+            <a href={this.props.link} target="_blank">
+              <img className={styles.image} src={this.props.image} />
+            </a>
+          </div>
+          <div className={styles.computerStand} />
+          <div className={styles.computerFoot} />
+        </div>
+        <div className={styles.mobileSection}>
+          <div className={styles.imageContainerMobile}>
+            <a href={this.props.link} target="_blank">
+              <img className={styles.imageMobile} src={this.props.mobileImage} />
+            </a>
+            <div className={styles.mobileButton} />
+          </div>
+        </div>
       </div>
-      <p className={styles.body}>{this.props.description}</p>
+
+      <section>
+        <h2 className={styles.header}>{this.props.header}</h2>
+        <p className={styles.body}>{this.props.description}</p>
+      </section>
       <p className={styles.footNote}>{this.props.tech}</p>
     </div>)
   }
--- universal/layouts/Portfolio/component.js
+++ universal/layouts/Portfolio/component.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 import ProjectSection from 'components/ProjectSection'
 
@@ -6,6 +7,8 @@ import styles from './styles'
 import contentStyles from 'styles/content.css'
 
 import imagePlanMade from './assets/planmadequotes.png'
+import imagePlanMadeMobile from './assets/planmademobile.png'
+import imageMusicRehab from './assets/musicrehab.png'
 
 class Portfolio extends React.Component {
 
@@ -18,9 +21,21 @@ class Portfolio extends React.Component {
           header={'PlanMade Life Insurance'}
           link={'http://www.myplanmade.com/'}
           image={imagePlanMade}
+          mobileImage={imagePlanMadeMobile}
           description={'Chris Thompson is a young aspiring entrepreneur working in the life insurance space. I was able to get him a fully functional MVP app to help him validate his business model and catch the attention of investors. This project involved an integration with a 3rd party service for quote data, automated emails to help get users engaged with the bussiness, and an interactive form for modifying quote data.'}
-          tech={'This is a two piece app, the frontend and backend are completely decoupled. The frontend is a react based static site. The backend is a Ruby on Rails API server.'}
+          tech={'This is a two piece app, the frontend and backend are completely decoupled. The frontend is a mobile-friendly react based static site. The backend is a Ruby on Rails API server.'}
         />
+        <ProjectSection
+          header={'Musih Therapy Stroke Rehabilitation'}
+          link={'http://musicrehabapp.herokuapp.com/'}
+          image={imageMusicRehab}
+          description={'Martha Summa has a vision of better rehabilitation tools...'}
+          tech={'This is a two piece app, the frontend and backend are completely decoupled. The frontend is a mobile-friendly react based static site. The backend is a Ruby on Rails API server.'}
+        />
+        <div style={{textAlign: 'center'}}>
+          <p className={contentStyles.body}>I've helped many entrepreneurs in realizing their dreams. You could be next.</p>
+          <Link to='contact' className={contentStyles.link}>Let's work together</Link>
+        </div>
       </main>
     )
   }
